cf9dcc8c957f0637480dd3ee261f3b3f9a4a156c
Hakan Yalniz <hakan_yalniz@live.com>
2023-08-03 17:46:44 +0300

    Cleaned up some code and added comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
+// All of the divs are created inside the container
 const canvasContainer = document.getElementById("canvas-container");
 
+// Is responsible for creating the divs with the given gridSize
+// The way the grids look is handled by CSS
 function createDiv(gridSize) {
   for (let i = 0; i < gridSize; i++) {
     for (let x = 0; x < gridSize; x++) {
@@ -10,14 +13,21 @@ function createDiv(gridSize) {
   }
 }
 
+// Is responsible for drawing, makes the backgroundColor to black
+// Later on add a way of changing the color from UI
+
 function canvasWrite(event) {
   console.log(event.currentTarget);
 
   event.currentTarget.style.backgroundColor = "black";
 }
 
+// Change the below parameter to change the gridSize
+// Later on add a way of changing the size from UI
 createDiv(16);
 
+// Adds the event listeners to each child element of the canvasContainer
+// and calls canvasWrite on them when event is triggered
 for (const child of canvasContainer.children) {
   child.addEventListener("mouseover", (event) => {
     canvasWrite(event);
