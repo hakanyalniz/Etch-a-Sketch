5c8fab836beb176692d789d31ca83848ea1013ac
Hakan Yalniz <hakan_yalniz@live.com>
2023-08-03 17:37:27 +0300

    Made use of element.children to get list of all children, then looped over them and added event listeners to each, which when activated turns the background black

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
 const canvasContainer = document.getElementById("canvas-container");
 
 function createDiv(gridSize) {
-  for (let i = 0; i <= gridSize; i++) {
-    let newDiv = document.createElement("div");
+  for (let i = 0; i < gridSize; i++) {
+    for (let x = 0; x < gridSize; x++) {
+      let newDiv = document.createElement("div");
 
-    canvasContainer.append(newDiv);
+      canvasContainer.append(newDiv);
+    }
   }
 }
 
+function canvasWrite(event) {
+  console.log(event.currentTarget);
+
+  event.currentTarget.style.backgroundColor = "black";
+}
+
 createDiv(16);
+
+for (const child of canvasContainer.children) {
+  child.addEventListener("mouseover", (event) => {
+    canvasWrite(event);
+  });
+}
