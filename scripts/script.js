79bdb726f507d4b516f833e15b7e230c74b272b5
Hakan Yalniz <hakan_yalniz@live.com>
2023-08-04 15:02:16 +0300

    Now the canvas only works when mouse is hold down instead of hover. Also the event propagation was used so only adding one event listener at canvas container was enough, removed adding event listeners to all of the div inside canvas container

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 // All of the divs are created inside the container
 const canvasContainer = document.getElementById("canvas-container");
+// Is needed to check whether the user has pressed down the mouse button or released it yet
+// We need this so the canvas only draws when the mouse is hold down
+let mouseCondition = false;
 
 // Is responsible for creating the divs with the given gridSize
 // The way the grids look is handled by CSS
@@ -7,6 +10,7 @@ function createDiv(gridSize) {
   for (let i = 0; i < gridSize; i++) {
     for (let x = 0; x < gridSize; x++) {
       let newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "canvas-div");
 
       canvasContainer.append(newDiv);
     }
@@ -19,17 +23,31 @@ function createDiv(gridSize) {
 function canvasWrite(event) {
   console.log(event.currentTarget);
 
-  event.currentTarget.style.backgroundColor = "black";
+  event.target.style.backgroundColor = "black";
 }
 
 // Change the below parameter to change the gridSize
 // Later on add a way of changing the size from UI
+// When the below changes the div box sizes remain same so it overflows
+// Need to find a way to fix that
 createDiv(16);
 
-// Adds the event listeners to each child element of the canvasContainer
-// and calls canvasWrite on them when event is triggered
-for (const child of canvasContainer.children) {
-  child.addEventListener("mouseover", (event) => {
+// New way of adding event listener. Way more efficient than the previous way
+// which added event listener to every div created under the canvas
+// this method makes use of event propagation, the bubble up stuff
+// and only changes the color when the class matches
+canvasContainer.addEventListener("mouseover", function (event) {
+  if (event.target.className === "canvas-div" && mouseCondition == true) {
     canvasWrite(event);
-  });
-}
+  }
+});
+
+// Checks mouse condition so we can only draw when the mouse is pressed down,
+// which is indicated by mouseCondition
+canvasContainer.addEventListener("mousedown", (event) => {
+  event.preventDefault();
+  mouseCondition = true;
+});
+canvasContainer.addEventListener("mouseup", () => {
+  mouseCondition = false;
+});
